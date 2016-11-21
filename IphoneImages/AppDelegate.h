167b494c3de7a9560a80590a4b53f424426710f4
Hamza Lakhani <hamzalakhani10@gmail.com>
2016-11-21 11:16:59 -0500

    Initial Commit

---
+++ IphoneImages/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IphoneImages
+//
+//  Created by Hamza Lakhani on 2016-11-21.
+//  Copyright © 2016 Hamza Lakhani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IphoneImages/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IphoneImages
+//
+//  Created by Hamza Lakhani on 2016-11-21.
+//  Copyright © 2016 Hamza Lakhani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
